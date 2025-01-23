743461ddc60cea981b3f089143e6385821c3b7a2
KitCat <katsulynx@gmail.com>
2025-01-23 20:15:31 +0000

    Prevent nested `parse` calls from triggering roll grammar evaluation References #5

--- modules/dice/rollParse.mjs
+++ modules/dice/rollParse.mjs
@@ -1,17 +1,19 @@
 import {RollGrammar} from "./_module.mjs";
 
 export default function parse(wrapped, formula, data = {}) {
+    const trace = new Error().stack;
+    if(trace.includes('Roll._evaluateAST') || trace.includes('Roll.instantiateAST')) return wrapped(formula, data);
+
     try {
         if (formula) {
-            const oldFormula = formula
+            const oldFormula = formula;
             const start = Date.now();
             formula = RollGrammar.parse(formula);
-            const executionTime = Date.now() - start;
             if (oldFormula.replaceAll(" ", "") !== formula.replaceAll(" ", "")) {
                 console.debug("Better Roll Formulas | formula transformed", {
                     oldFormula: oldFormula,
                     newFormula: formula,
-                    executionTime: executionTime + " ms"
+                    executionTime: (Date.now() - start) + " ms"
                 })
             }
         }
