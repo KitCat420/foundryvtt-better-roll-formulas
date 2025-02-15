012222ca35be18d57d56d239a485b0183c230f52
KitCat <katsulynx@gmail.com>
2025-02-15 15:18:34 +0000

    Add formula transformation cache, drastically improving performance

--- modules/dice/rollParse.mjs
+++ modules/dice/rollParse.mjs
@@ -1,6 +1,10 @@
 import {RollGrammar} from "./_module.mjs";
 
+const formulaCache = new Map();
+
 export default function parse(wrapped, formula, data = {}) {
+    if(formula && formulaCache.has(formula)) return wrapped(formulaCache.get(formula), data);
+
     const trace = new Error().stack;
     if(trace.includes('Roll._evaluateAST') || trace.includes('Roll.instantiateAST')) return wrapped(formula, data);
 
@@ -16,10 +20,12 @@ export default function parse(wrapped, formula, data = {}) {
                     executionTime: (Date.now() - start) + " ms"
                 })
             }
+            formulaCache.set(oldFormula, formula);
         }
     } catch (e) {
         console.warn("Better Roll Formulas | Failed to parse formula:", formula);
         console.error("Better Roll Formulas | ", e)
+        formulaCache.set(formula, formula);
     }
     return wrapped(formula, data);
 }
